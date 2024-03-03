504fbd82d5c97a6afac7476253c74180684f9a57
Long <lrng159@gmail.com>
2024-03-03 20:17:46 +0700

    update gh-pages

--- 02 - Reactjs-Building the Frontend/app-frontend/src/users/components/UsersItem/UsersItem.jsx
+++ 02 - Reactjs-Building the Frontend/app-frontend/src/users/components/UsersItem/UsersItem.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import Avatar from '../../../shared/components/UIElements/Avatar'
+import { Link } from 'react-router-dom'
+import Card from '../../../shared/components/UIElements/Card'
 import './UsersItem.css'
 function UsersItem(props) {
   return (
     <li className='user-item'>
-        <div className='user-item__content'>
-            <div className='user-item__image'>
-                <img src={props.image} alt={props.name} />
-            </div>
+          <Card className='user-item__content'>
+            <Link to={`/${props.id}/places`}>
+              <div className='user-item__image'>
+                  <Avatar image={props.image} alt={props.name} />
+              </div>
 
-            <div className="user-item__info">
-                <h2>{props.name}</h2>
-                <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-            </div>
-        </div>
+              <div className="user-item__info">
+                  <h2>{props.name}</h2>
+                  <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+              </div>
+              </Link>
+            </Card>
     </li>
   )
 }
