434eeca833bee8d92fca5710a44ed424a6797a25
Long <lrng159@gmail.com>
2024-03-15 21:47:06 +0700

    add place form

--- 02 - Reactjs-Building the Frontend/app-frontend/src/places/pages/NewPlace.jsx
+++ 02 - Reactjs-Building the Frontend/app-frontend/src/places/pages/NewPlace.jsx
@@ -1,10 +1,67 @@
 import Input from '../../shared/components/FormElements/Input'
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import { useCallback, useReducer } from 'react'
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import Button from '../../shared/components/FormElements/Button';
 import './NewPlace.css'
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      }
+    default:
+      return state
+  }
+}
+
 function NewPlace() {
+  const [formState, dispatch] =  useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    isValid: false
+  })
+  const inputHander = useCallback((id, value, isValid) => {
+dispatch({
+    type: 'INPUT_CHANGE',
+    value: value,
+    isValid: isValid,
+    inputId: id})
+  }, [])
+
+  const placeSubmitHandler = event => {
+    event.preventDefault()
+    console.log(formState.inputs)
+  }
+
+
   return (
-    <form className="place-form">
-      <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" onInput={inputHander} />
+      <Input id="description" element="textarea" type="text" label="Description" validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description (at lease 5 characters)" onInput={inputHander} />
+      <Input id="adress" element="input" type="text" label="Address" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid address" onInput={inputHander} />
+      <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
   )
 }
--- 02 - Reactjs-Building the Frontend/app-frontend/src/shared/components/FormElements/Input.jsx
+++ 02 - Reactjs-Building the Frontend/app-frontend/src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
 import { validate } from '../../util/validators'
 import './Input.css'
 
@@ -25,6 +25,13 @@ function Input(props) {
         isTouched: false,
         isValid: false
     })
+
+      const { id, onInput } = props
+      const {value, isValid} = inputState
+    useEffect(() => {
+      onInput(id, value, isValid)
+    }, [id, value, isValid, onInput])
+
 
     const changeHandler = event => {
         dispatch({
